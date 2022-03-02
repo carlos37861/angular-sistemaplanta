0cd6c609a8ac99250b0dd1926466c945fbb00efc
Carlos Perez <carlos3786@hotmail.com>
2022-03-02 15:51:07 -0500

    cambio2

---
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import swal from'sweetalert2';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acciones'];
+  dataSource = ELEMENT_DATA;
+  constructor(private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+  prueba(){
+    // swal.fire({
+    //   position: 'center',
+    //   icon: 'success',
+    //   title: 'Your work has been saved',
+    //   showConfirmButton: false,
+    //   timer: 1500
+    // })
+    this.toastr.success('success','Se registro bien')
+  }
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SiderbarComponent } from './components/siderbar/siderbar.component';
+import { MatDividerModule} from '@angular/material/divider'
+import { MatToolbarModule} from '@angular/material/toolbar'
+import { MatIconModule} from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
+import{FlexLayoutModule} from '@angular/flex-layout';
+import{MatMenuModule} from '@angular/material/menu';
+import{MatListModule} from '@angular/material/list'
+import {RouterModule} from '@angular/router';
+import {MatTableModule} from '@angular/material/table';
+import  {  ToastrModule  }  from  'ngx-toastr' ;
+import {MatTooltipModule} from '@angular/material/tooltip';
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent, 
+    SiderbarComponent,
+    
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatListModule,
+    RouterModule,
+    MatTableModule,
+    MatTooltipModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    }),
+   
+  ],
+  exports:[
+    HeaderComponent,
+    FooterComponent,
+    SiderbarComponent,
+    MatTableModule,
+     MatIconModule, // <-- here
+     ToastrModule,
+     MatTooltipModule
+  ]
+})
+export class SharedModule { }
