fe9a9b4a3bbe2e6397e08858eb6d8a04de90dadd
Carlos Perez <carlos3786@hotmail.com>
2022-03-02 15:44:03 -0500

    save arezzo files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
-import { NgModule } from '@angular/core';
+import { Component, DefaultIterableDiffer, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DefaultModule } from './layouts/default/default.module';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { PostsComponent } from './modules/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {
+  path:'',
+  component:DefaultComponent,
+  children:[{
+    path:'',
+    component:DashboardComponent
+  },{
+  path:'posts',
+  component: PostsComponent 
+  },
+  {
+    path:'**',
+    redirectTo:'',
+    pathMatch:'full'
+  }
+]
+},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
